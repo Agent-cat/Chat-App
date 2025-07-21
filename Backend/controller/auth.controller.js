b2f6728df43b4ae1d830bcfe49128293516ec174
Agent-cat <2300031042@kluniversity.in>
2025-07-21 20:57:31 +0530

    logout controller

--- Backend/controller/auth.controller.js
+++ Backend/controller/auth.controller.js
@@ -42,28 +42,34 @@ export const Signup = async (req, res) => {
   }
 };
 export const Login = async (req, res) => {
-  const {email,password}=req.body();
-  if(!email||!password){
-    return res.status(400).json({message:"Please fill all the required fields "})
+  const { email, password } = req.body();
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Please fill all the required fields " });
   }
   try {
-    const userdata= await User.findOne({email});
-  if(!userdata)return res.json({message:"Invalid credentials"});
-  const ispasswordcorrect =bcrypt.compare(password,userdata.password);
-  if(!ispasswordcorrect)return res.json({message:"invalid credentials"});
-  generateToken(userdata._id,res);
+    const userdata = await User.findOne({ email });
+    if (!userdata) return res.json({ message: "Invalid credentials" });
+    const ispasswordcorrect = bcrypt.compare(password, userdata.password);
+    if (!ispasswordcorrect) return res.json({ message: "invalid credentials" });
+    generateToken(userdata._id, res);
 
-  res.json({
-     _id: userdata._id,
-        fullname: userdata.fullname,
-        email: userdata.email,
-        profilepic: userdata.profilepic,
-  })
-    
+    res.json({
+      _id: userdata._id,
+      fullname: userdata.fullname,
+      email: userdata.email,
+      profilepic: userdata.profilepic,
+    });
   } catch (error) {
-    res.json({message:"Internal server error"});
+    res.json({ message: "Internal server error" });
   }
-  
-  
 };
-export const Logout = async (req, res) => {};
+export const Logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+  } catch (error) {
+    console.log(`Error in logout controller ${error.message}`);
+    res.status(404).json({ message: "Internal server error" });
+  }
+};
